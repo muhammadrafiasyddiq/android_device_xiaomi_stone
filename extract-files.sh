376669694ae45399668c03dd8a462f1a2c745880
SamarV-121 <samarvispute121@pm.me>
2022-08-20 15:57:46 +0100

    Stone: Use libutils-v32 for libmtkcam_stdutils.so
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Change-Id: I098c7727e114781775d1b1d8f4e2c9e64bc94e3a

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,9 @@ function blob_fixup() {
             vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
             "$PATCHELF" --replace-needed "libhidlbase.so" "libhidlbase_shim.so" "$2"
             ;;
+            vendor/lib64/mt6789/libmtkcam_stdutils.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
     esac
 }
 
