18df4ab38c2011ebf619f1554e925d24fb1c0737
ChrisCatto <chriscatto08@gmail.com>
2024-06-22 16:29:50 +0200

    Rock : Link mediatek camera provider against v32 `libutils`
    * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0
      which isn't already owned'
    * Required after U QPR3
    
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -79,7 +79,8 @@ function blob_fixup() {
             vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
             "$PATCHELF" --replace-needed "libhidlbase.so" "libhidlbase_shim.so" "$2"
             ;;
-            vendor/lib64/mt6789/libmtkcam_stdutils.so)
+            vendor/lib64/libmtkcam_stdutils.so|\
+        vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
             system_ext/lib64/libsource.so)
