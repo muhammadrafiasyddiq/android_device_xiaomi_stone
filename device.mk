0e1e8f3cc9d545f3c4317fa37244cb52b35f77e8
Android Build Bot Worker <>
2024-06-22 21:58:02 +0700

    Rock : Overlay Move Overlay To rro_overlay

--- device.mk
+++ device.mk
@@ -359,16 +359,13 @@ PRODUCT_PACKAGES += \
     SecureElement
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage 
-
 PRODUCT_PACKAGES += \
     WifiResOverlayStone \
     SystemUIOverlayStone \
-    SettingsOverlayStone
-
-
+    SettingsOverlayStone \
+    FrameworkOverlayStone \
+    TelephonyOverlayStone \
+    CarrierConfigOverlayStone
 
 # Rootdir
 PRODUCT_PACKAGES += \
